c9f718ab2a5bddfb3dfa2147c4a591ba8f85359d
siddhantsorann <siddhantsorann@gmail.com>
2021-02-15 16:53:32 +0530

    resolving pr issues

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/config/DataFlowControllerAutoConfiguration.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/config/DataFlowControllerAutoConfiguration.java
@@ -22,7 +22,6 @@ import java.util.concurrent.ForkJoinPool;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
